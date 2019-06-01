38fb6629d4743c27aa076670f1b952ab51686166
reyeskevin9767 <reyeskevin9767@gmail.com>
2019-06-01 12:45:25 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React , {Component} from 'react';
+import logo from './logo.svg';
+import Lottery from "./Lottery";
+import './App.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+          < Lottery />
+          < Lottery title="Mini Daily" numBalls={4} maxNum={10}/>
+        </div>
+    );
+  }
+}
+
+export default App;
